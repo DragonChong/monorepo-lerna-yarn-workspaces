cfea523c584e0a30635e8b7063a9f86b0c7b62c6
dragonchong <dragonchong628+npmjs@gmail.com>
2019-10-28 09:47:28 +0800

    try publish package

--- packages/button/src/index.js
+++ packages/button/src/index.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-      background: blue;
+      background: yellow;
       color: #fff;
       border-radius: 4px;
       cursor: pointer;
--- packages/input/src/index.story.js
+++ packages/input/src/index.story.js
@@ -4,5 +4,5 @@ import { storiesOf } from "@storybook/react";
 import Input from ".";
 
 storiesOf("Input", module).add("default", () => (
-      <Input placeholder="Yoyo World!" />
+      <Input placeholder="HiBye World!" />
 ));
