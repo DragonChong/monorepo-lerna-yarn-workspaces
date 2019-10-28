8fec91bf4d30a5e558743211bd3d4c47aa6ce877
dragonchong <dragonchong628+npmjs@gmail.com>
2019-10-28 11:22:55 +0800

    update button only

--- packages/app/src/App.js
+++ packages/app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import Input from "@dragonchong/test-lerna-input";
+// import Input from "@dragonchong/test-lerna-input";
 import Button from "@dragonchong/test-lerna-button";
 
 import "./App.css";
@@ -13,7 +13,7 @@ class App extends Component {
                               <img src={logo} className="App-logo" alt="logo" />
                               <h1 className="App-title">Welcome to React</h1>
                         </header>
-                        <Input placeholder="Hi~Bye World!" />
+                        {/* <Input placeholder="Hi~Bye World!" /> */}
                         <Button>Click Me!</Button>
                   </div>
             );
